24fd25a9ec319934407fca8713fc9f2e7cda4631
JosafatFlores <josafatfloresr@gmail.com>
2018-03-14 19:44:26 -0600

    Fucionamiento de contador de encendido de led

--- Puerta/Puerta.ino
+++ Puerta/Puerta.ino
@@ -2,37 +2,34 @@
     
     int contacto = 2;
     int led= 13; 
-    
-    void wake ()
+    int contador = 0;
+
+ void wake ()
     {
       // cancel sleep as a precaution
       sleep_disable();
       // precautionary while we do other stuff
-      detachInterrupt (0);
+      detachInterrupt (2);
     }  // end of wake
     
     void setup () 
       {
-      digitalWrite (2, HIGH);  // enable pull-up
-          // end of setup
-    
-    
+    Serial.begin(9600);
       pinMode(contacto,INPUT);
       pinMode(led, OUTPUT); 
-      ADCSRA = 0;  
+       ADCSRA = 0;  
       set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
       sleep_enable();
       attachInterrupt (contacto, wake, RISING);
-      EIFR = bit (INTF0);  
-        MCUCR = bit (BODS) | bit (BODSE);
-        MCUCR = bit (BODS); 
-        interrupts ();  
-      sleep_cpu ();   
+
+    interrupts ();  
+
     }
     
     void loop(){
       if (digitalRead(contacto)==HIGH){
-      
+        contador ++;
+        Serial.print(contador);
           digitalWrite(led,HIGH); 
           delay(2000) ;
       }else{
